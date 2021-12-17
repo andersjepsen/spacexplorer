eeb73461b8ae58d1326bb508b9b6e1431a7a6998
Anders Jepsen <hi@andersjepsen.com>
2021-12-17 10:39:44 +0100

    :zap: handle launches as offset pagination

--- apollo.client.ts
+++ apollo.client.ts
@@ -4,6 +4,8 @@ import {
   HttpLink,
   InMemoryCache,
 } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import { offsetLimitPagination } from "@apollo/client/utilities";
 import { withScalars } from "apollo-link-scalars";
 import { buildClientSchema, IntrospectionQuery } from "graphql";
 import introspection from "./api/introspection.json";
@@ -12,15 +14,33 @@ const schema = buildClientSchema(
   introspection as unknown as IntrospectionQuery
 );
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.error(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.error(`[Network error]: ${networkError}`);
+});
+
 const httpLink = new HttpLink({
   uri: process.env.NEXT_PUBLIC_API,
 });
 
-const link = ApolloLink.from([withScalars({ schema }), httpLink]);
+const link = ApolloLink.from([errorLink, withScalars({ schema }), httpLink]);
 
 const client = new ApolloClient({
   link,
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          launches: offsetLimitPagination(),
+        },
+      },
+    },
+  }),
 });
 
 export default client;
